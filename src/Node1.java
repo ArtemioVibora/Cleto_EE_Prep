a94c38860ad0c1a7e5d2c9e32646b7016bef9e2c
nina <juan.amado.sicat.cleto@gmail.com>
2025-08-14 20:54:41 +0800

    first

---
+++ src/Node1.java
@@ -0,0 +1,31 @@
+class Node
+{
+
+    private Node next;
+    private int value;
+
+    public Node(int value, Node next)
+    {
+        this.next = next;
+        this.value = value;
+    }
+
+    public Node getNext()
+    {
+        return next;
+    }
+
+    public int getValue()
+    {
+        return value;
+    }
+}
+
+
+public class Node1 {
+    public static void main(String[] a)
+    {
+
+    }
+
+}
